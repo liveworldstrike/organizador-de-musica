bcf1f57c57b431c5066dc1c8f8d2a5e0c85ed25b
liveworldstrike <lazodellamas12@gmail.com>
2015-12-19 12:40:12 +0100

    ​Métodos listFile y removeFile reescritos para evitar redundancia​

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -10,15 +10,19 @@ public class MusicOrganizer
 {
     // An ArrayList for storing the file names of music files.
     private ArrayList<String> files;
-        
+    //guarda si es true o false en los casos del metodo validindex;
+    private boolean validIndex;
+
     /**
      * Create a MusicOrganizer
      */
     public MusicOrganizer()
     {
         files = new ArrayList<String>();
+        validIndex = false;
+
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -27,7 +31,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -36,7 +40,7 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
@@ -48,7 +52,7 @@ public class MusicOrganizer
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
@@ -59,14 +63,32 @@ public class MusicOrganizer
             files.remove(index);
         }
     }
+
     /**
      * metodo para comprobar la organizacion y comprobacion de los incides si son correctos.
      */
     public void checkIndex(int index)
     {
         if(index < 0 || index >= files.size()){
-           System.out.println("Error rango dado no es correcto,introducir rango de 0 a " + files.size());
-            
+            System.out.println("Error rango dado no es correcto,introducir rango de 0 a " + files.size());
+
         }
     }
+
+    /**
+     *  metodo que devuelve un valor booleano en función de si su parámetro entero es un índice válido o no para el atributo files.
+     *  
+     */
+    public void validIndex(int index)
+    {
+        if(index < 0 || index >= files.size()){
+            validIndex=false;
+
+        }
+        else{
+            validIndex=true;
+        }
+
+    }
+
 }
